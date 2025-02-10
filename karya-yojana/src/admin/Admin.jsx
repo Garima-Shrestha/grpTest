df702ecaac05d565f38012671a61c9e4cd86df08
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-02-10 18:31:27 +0545

    vandina

--- karya-yojana/src/admin/Admin.jsx
+++ karya-yojana/src/admin/Admin.jsx
@@ -1,12 +1,18 @@
 import React, { Suspense, lazy } from 'react';
-
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 const Dash = lazy(() => import("./AdminDashboard.jsx"));
 
 const Admin = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+   const routeComponents = {
+       '/useradmin': <Dash/>,
+     };
     return (
         <>
             <Suspense fallback={<div>Loading...</div>}>
-                <Dash />
+            {routeComponents[location.pathname] || <div>404 Page Not Found</div>}
             </Suspense>
         </>
     );
